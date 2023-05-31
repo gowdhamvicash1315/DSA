46635ea8f9573047423cf96f5c309d90d890973c
gowdhamvicash1315 <131777570+gowdhamvicash1315@users.noreply.github.com>
2023-06-01 00:06:36 +0530

    Add files via upload

---
+++ 29may.js
@@ -0,0 +1,77 @@
+function printPattern() {
+  const pattern = [
+    "*****",
+    "*   *",
+    "*   *",
+    "*   *",
+    "*****"
+  ];
+
+  for (let i = 0; i < pattern.length; i++) {
+    console.log(pattern[i]);
+  }
+}
+
+printPattern();
+
+
+function printPattern1() {
+  const pattern1 = [
+    "***********",
+    "*",
+    "*",
+    "***********",
+    "*",
+    "*",
+    "***********"
+  ];
+
+  for (let i = 0; i < pattern1.length; i++) {
+    console.log(pattern1[i]);
+  }
+}
+
+printPattern1();
+
+
+function printPattern2() {
+  const pattern2 = [
+    "***********",
+    "          *",
+    "          *",
+    "***********",
+    "          *",
+    "          *",
+    "***********"
+  ];
+
+  for (let i = 0; i < pattern2.length; i++) {
+    console.log(pattern2[i]);
+  }
+}
+
+printPattern2();
+
+
+
+function printPattern3() {
+  const pattern3 = [
+    "***********",
+    "     *",
+    "     *",
+    "     *",
+    "***********",
+    "     *",
+    "     *",
+    "     *",
+    "     *",
+    "***********"
+  ];
+
+  for (let i = 0; i < pattern3.length; i++) {
+    console.log(pattern3[i]);
+  }
+}
+
+printPattern3();
+
